3abccf5fdfdd3b5946722a03962a0e76009b6e38
Harriish Lange <harishlange24@gmail.com>
2026-08-20 20:43:25 +0530

    Update mailer.js

--- Backend/src/config/mailer.js
+++ Backend/src/config/mailer.js
@@ -4,21 +4,23 @@ import dns from "dns";
 
 dotenv.config();
 
-console.log("🔥 MAILER.JS LOADED");
-
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+
+    requireTLS: true,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
 
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
+    connectionTimeout: 15000,
+    greetingTimeout: 15000,
+    socketTimeout: 20000,
 });
 
 transporter.verify()
