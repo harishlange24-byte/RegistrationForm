b7718e68d558a9f35088b30107d53628c22e0c62
Harish Lange <harishlange24@gmail.com>
2026-08-20 18:28:22 +0530

    Update registration email and UI

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import UserModel from "../models/user.model.js";
+import transporter from "../config/mailer.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 async function Registration(req, res) {
     try {
@@ -18,6 +21,38 @@ async function Registration(req, res) {
             contact,
             address,
         });
+         console.log("EMAIL USER:", process.env.EMAIL_USER);
+             console.log("EMAIL ADMIN:", process.env.EMAIL_ADMIN);
+
+       const info =  await transporter.sendMail(
+            
+            {
+             
+            from:process.env.EMAIL_USER,
+            to:[
+                "harishlange24@gmail.com",
+                "amandasgupta975@gmail.com"
+            ],
+
+            subject : "New Garba Registration",
+
+            html:`
+            <h2>New Garba Registration</h2>
+            
+            <p><strong>Name : </strong> ${name}</p>
+             <p><strong>Age : </strong> ${age}</p>
+              <p><strong>Gender : </strong> ${gender}</p>
+               <p><strong>Contact : </strong> ${contact}</p>
+                <p><strong>Address : </strong> ${address}</p>
+
+                <hr />
+
+                <p>A new user has registered for the Garba event.</p>
+                `,
+        })
+        console.log("Email sent:", info.messageId);
+console.log("Accepted:", info.accepted);
+console.log("Rejected:", info.rejected);
 
         return res.status(201).json({
             success: true,
