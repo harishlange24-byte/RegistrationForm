2494038ec20a6f01dd838cba7682f48ed29c6952
Harriish Lange <harishlange24@gmail.com>
2026-08-20 20:36:56 +0530

    Update mailer.js

--- Backend/src/config/mailer.js
+++ Backend/src/config/mailer.js
@@ -1,30 +1,36 @@
-console.log("🔥 MAILER.JS LOADED");
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-
+console.log("🔥 MAILER.JS LOADED");
 console.log("EMAIL USER:", process.env.EMAIL_USER);
 console.log("EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 });
+
 console.log("🔥 TRANSPORTER CREATED");
-transporter.verify((error, success) => {
 
-    if (error) {
-        console.log("EMAIL CONFIGURATION ERROR:", error);
-    } else {
-        console.log("EMAIL SERVER IS READY");
-    }
-
-});
+transporter.verify()
+    .then(() => {
+        console.log("✅ EMAIL SERVER IS READY");
+    })
+    .catch((error) => {
+        console.log("❌ EMAIL VERIFY FAILED");
+        console.log(error);
+    });
 
 export default transporter;
