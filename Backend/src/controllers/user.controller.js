5ddccf53c1b3743494dc5c6a5c84bd569d90fab8
Harriish Lange <harishlange24@gmail.com>
2026-08-20 20:05:51 +0530

    Refactor email sending in user registration
    Refactor email sending logic to use promise chaining and improve logging.

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -24,37 +24,40 @@ async function Registration(req, res) {
          console.log("EMAIL USER:", process.env.EMAIL_USER);
              console.log("EMAIL ADMIN:", process.env.EMAIL_ADMIN);
 
-       const info =  await transporter.sendMail(
-            
-            {
-             
-            from:process.env.EMAIL_USER,
-            to:process.env.EMAIL_ADMIN,
-
-            subject : "New Garba Registration",
-
-            html:`
-            <h2>New Garba Registration</h2>
-            
-            <p><strong>Name : </strong> ${name}</p>
-             <p><strong>Age : </strong> ${age}</p>
-              <p><strong>Gender : </strong> ${gender}</p>
-               <p><strong>Contact : </strong> ${contact}</p>
-                <p><strong>Address : </strong> ${address}</p>
-
-                <hr />
-
-                <p>A new user has registered for the Garba event.</p>
-                `,
-        })
-        console.log("Email sent:", info.messageId);
-console.log("Accepted:", info.accepted);
-console.log("Rejected:", info.rejected);
-
+      
         return res.status(201).json({
             success: true,
             message: "User registered successfully",
             user,
+        });
+
+           // 3. Email separately
+        transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_ADMIN,
+            subject: "New Garba Registration",
+
+            html: `
+                <h2>New Garba Registration</h2>
+
+                <p><strong>Name:</strong> ${name}</p>
+                <p><strong>Age:</strong> ${age}</p>
+                <p><strong>Gender:</strong> ${gender}</p>
+                <p><strong>Contact:</strong> ${contact}</p>
+                <p><strong>Address:</strong> ${address}</p>
+
+                <hr />
+
+                <p>A new user has registered for the Garba event.</p>
+            `,
+        })
+        .then((info) => {
+            console.log("EMAIL SENT:", info.messageId);
+            console.log("ACCEPTED:", info.accepted);
+            console.log("REJECTED:", info.rejected);
+        })
+        .catch((error) => {
+            console.error("EMAIL FAILED:", error);
         });
     } catch (err) {
         if (err.name === "ValidationError") {
